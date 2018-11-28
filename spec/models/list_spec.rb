3a19b7ffad5e6ec37d56230720f5ba18788e7f24
Peter Jang <peter.x.jang@gmail.com>
2018-11-27 21:03:28 -0600

    Add test for first list method

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#complete_all_tasks!" do
+    it "should mark all tasks from a list as complete" do
+      list = List.create(name: "Chores")
+      Task.create(list_id: list.id, complete: false)
+      Task.create(list_id: list.id, complete: false)
+      list.complete_all_tasks!
+      list.tasks.each do |task|
+        expect(task.complete).to eq(true)
+      end
+    end
+  end
 end
